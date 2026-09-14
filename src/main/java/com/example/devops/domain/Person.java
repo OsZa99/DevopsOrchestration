d7953c26b86b428cc8181411d6b52c09ebd65f44
Ossama Zarani <ossama.zarani10@gmail.com>
2026-09-14 16:28:54 +0200

    feat: Import an old API REST Java project

---
+++ src/main/java/com/example/devops/domain/Person.java
@@ -0,0 +1,56 @@
+package com.example.devops.domain;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+
+@Entity
+@Table(name = "personnes")
+public class Person implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    @Column(nullable = false)
+    private String lastname;
+    
+    @Column(nullable = false)
+    private String firstname;
+    
+    private int age;
+    
+    private String email;
+
+    public Person() {
+    }
+
+    public Person(String lastname, String firstname, int age, String email) {
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.age = age;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+}
